1ff85f4634babbefd02a7c5f2d31d42573ec29c5
aklkbqx <tasekaya222@gmail.com>
2025-01-18 00:08:40 +0700

    first upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Kanit } from "next/font/google";
+import "./globals.css";
+
+const kanitFont = Kanit({
+  subsets: ["latin"],
+  weight: ["200", "400", "600", "800"]
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio aklkbqx",
+  description: "Portfolio aklkbqx",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={kanitFont.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
